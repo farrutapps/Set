fe6b259a040f7903c44346be43ffd2ab88331086
Sebastian.Ratz <sebastian.ratz.s@gmail.com>
2016-03-26 21:52:54 +0100

    fk missing activities in manifest

---
+++ app/src/main/java/com/farrutapps/set/view/BoardActivity.java
@@ -0,0 +1,15 @@
+package com.farrutapps.set.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.farrutapps.set.R;
+
+public class BoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+    }
+}
---
+++ app/src/main/java/com/farrutapps/set/view/PlayActivity.java
@@ -0,0 +1,7 @@
+package com.farrutapps.set.view;
+
+/**
+ * Created by Sebastian on 26.03.16.
+ */
+public class PlayActivity {
+}
