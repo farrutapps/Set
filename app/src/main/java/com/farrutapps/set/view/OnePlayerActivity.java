2c939bbd031283c320cc2dea7235963dcaa1893e
Sebastian.Ratz <sebastian.ratz.s@gmail.com>
2016-03-28 22:28:45 +0200

    fk xml-activities designed. close branch.

--- app/src/main/java/com/farrutapps/set/view/OnePlayerActivity.java
+++ app/src/main/java/com/farrutapps/set/view/OnePlayerActivity.java
@@ -6,11 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import com.farrutapps.set.R;
 
 
-public class PlayActivity extends AppCompatActivity {
+public class OnePlayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play);
+        setContentView(R.layout.activity_one_player);
     }
 }
