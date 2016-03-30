a1eb462360564186ece03c5c066b0310964b8c80
Sebastian.Ratz <sebastian.ratz.s@gmail.com>
2016-03-30 10:57:59 +0200

    fk buttons connected to activities. working on fks phone.

--- app/src/main/java/com/farrutapps/set/view/HomeActivity.java
+++ app/src/main/java/com/farrutapps/set/view/HomeActivity.java
@@ -1,15 +1,38 @@
 package com.farrutapps.set.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.farrutapps.set.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Button btnPlay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        this.initResources();
+        this.setResources();
+    }
+
+    private void initResources(){
+        this.btnPlay = (Button) findViewById(R.id.btn_play);
+    }
+
+    private void setResources(){
+        this.btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO general function in Controller ?
+                Intent intent = new Intent(HomeActivity.this, PlayActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/farrutapps/set/view/OnePlayerActivity.java
+++ app/src/main/java/com/farrutapps/set/view/OnePlayerActivity.java
@@ -1,16 +1,59 @@
 package com.farrutapps.set.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.farrutapps.set.R;
 
 
 public class OnePlayerActivity extends AppCompatActivity {
 
+    private Button btnSoloMode, btnTimeRace, btnVsCpu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_player);
+
+        this.initResources();
+        this.setResources();
+    }
+
+    private void initResources(){
+        this.btnSoloMode = (Button) findViewById(R.id.btn_solomode);
+        this.btnTimeRace = (Button) findViewById(R.id.btn_timerace);
+        this.btnVsCpu = (Button) findViewById(R.id.btn_vscpu);
+    }
+
+    private void setResources(){
+        this.btnSoloMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO general function in Controller ?
+                Intent intent = new Intent(OnePlayerActivity.this, BoardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        this.btnTimeRace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO general function in Controller ?
+                Intent intent = new Intent(OnePlayerActivity.this, BoardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        this.btnVsCpu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO general function in Controller ?
+                Intent intent = new Intent(OnePlayerActivity.this, BoardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
